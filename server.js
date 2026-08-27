62dcfc48cbb1613e5afc755f334f14324218a809
Sarvesh Padmanabhan <sarvesh27.padmanabhan@gmail.com>
2026-08-27 19:44:57 +0530

    feat: optimize SEO and add sitemap support
    - Enhance meta tags for better search indexing.
    - Add sitemap.xml route to express server.
    - Update robots.txt to include sitemap.
    - Add lint script to package.json.

--- server.js
+++ server.js
@@ -67,6 +67,11 @@ app.get('/robots.txt', (req, res) => {
   res.sendFile(path.join(__dirname, 'robots.txt'));
 });
 
+app.get('/sitemap.xml', (req, res) => {
+  res.type('application/xml');
+  res.sendFile(path.join(__dirname, 'sitemap.xml'));
+});
+
 // Serve main page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
